026b3e04b138ab5d8a107e3d0484245a9773baaa
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-25 14:13:11 -0600

    created show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.all 
+    @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
 
 end
