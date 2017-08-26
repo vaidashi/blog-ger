d5a46d922b14c503a91706dd65de4284d9a7bfc6
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-26 12:51:34 -0600

    added edit ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
 
   def index
     @articles = Article.all
@@ -15,12 +16,25 @@ class ArticlesController < ApplicationController
   def create
     @article  = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
-  def destroy 
-    @article = Article.find(params[:id])
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to articles_path
   end
 
@@ -28,5 +42,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
